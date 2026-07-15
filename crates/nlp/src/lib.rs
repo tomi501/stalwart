6f20533c44ca211d88beff68bafe9ddfef23cc56
tomi <14106024+tomi501@users.noreply.github.com>
2026-07-15 20:37:37 +0300

    Delete crates directory

--- crates/nlp/src/lib.rs
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2020 Stalwart Labs LLC <hello@stalw.art>
- *
- * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-SEL
- */
-
-pub mod classifier;
-pub mod language;
-pub mod tokenizers;
